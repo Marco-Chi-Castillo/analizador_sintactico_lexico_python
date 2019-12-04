1b2922d405db87541dc30fdd700145b1accb68d0
RicardoIsmaelAlbornozChin <ismaeldalbornoz2098@gmail.com>
2019-12-04 15:30:24 -0600

    Completando lexer

--- js_lexer.py
+++ js_lexer.py
@@ -251,5 +251,273 @@ def t_REPLACE(t):
 def t_SUBSTR(t):
     r'substr'
     return t
-# Mi parte arriba
+    
+def t_RETURN(t):
+    r'return'
+    return t
+
+def t_PROMPT(t):
+    r'prompt'
+    return t
+
+def t_ALERT(t):
+    r'alert'
+    return t
+
+def t_OPERATION_TERNARIO(t):
+    r'\?'
+    return t
+
+def t_ARRAY(t):
+    r'Array'
+    return t
+
+def t_BREAK(t):
+    r'break'
+    return t
+
+def t_CASE(t):
+    r'case'
+    return t
+
+def t_CATCH(t):
+    r'catch'
+    return t
+
+def t_CLASS(t):
+    r'class'
+    return t
+
+def t_DEFAULT(t):
+    r'default'
+    return t
+
+def t_DO(t):
+    r'\bdo\b'
+    return t
+
+def t_ELSE(t):
+    r'else'
+    return t
+
+def t_ELSEIF(t):
+    r'elseif'
+    return t
+
+def t_ENDSWITCH(t):
+    r'endswitch'
+    return t
+
+def t_EVAL(t):
+    r'eval'
+    return t
+
+def t_EXTENDS(t):
+    r'extends'
+    return t
+
+def t_FOR(t):
+    r'for'
+    return t
+
+def t_FUNCTION(t):
+    r'function'
+    return t
+
+def t_IF(t):
+    r'if'
+    return t
+
+def t_IMPLEMENTS(t):
+    r'implements'
+    return t
+
+def t_INCLUDE(t):
+    r'include'
+    return t
+
+def t_INSTANCEOF(t):
+    r'instanceof'
+    return t
+
+def t_INTERFACE(t):
+    r'interface'
+    return t
+
+def t_OR(t):
+    r'or|\|\||OR'
+    return t
+
+def t_PRINT(t):
+    r'print'
+    return t
+
+def t_PRIVATE(t):
+    r'private'
+    return t
+
+def t_PROTECTED(t):
+    r'protected'
+    return t
+
+def t_PUBLIC(t):
+    r'public'
+    return t
+
+def t_REQUIRE(t):
+    r'require'
+    return t
+
+def t_STATIC(t):
+    r'static'
+    return t
+
+def t_SWITCH(t):
+    r'switch'
+    return t
+
+def t_THROW(t):
+    r'throw'
+    return t
+
+
+def t_TRY(t):
+    r'try'
+    return t
+
+def t_USE(t):
+    r'use'
+    return t
+
+def t_WHILE(t):
+    r'while'
+    return t
+
+def t_TRUE(t):
+    r'true'
+    return t
+
+def t_FALSE(t):
+    r'false'
+    return t
+
+# RE SYMBOLS
+t_PLUS      = r'\+'
+t_MINUS     = r'-'
+t_TIMES     = r'\*'
+t_DIVIDE    = r'/'
+t_EQUAL     = r'='
+t_DISTINT   = r'!'
+t_LESS      = r'<'
+t_GREATER   = r'>'
+t_SEMI      = r';'
+t_COMMA     = r','
+t_LPAREN    = r'\('
+t_RPAREN    = r'\)'
+t_LBRACKET  = r'\['
+t_RBRACKET  = r'\]'
+t_LBLOCK    = r'{'
+t_RBLOCK    = r'}'
+t_COLON     = r':'
+t_AMPERSANT = r'\&'
+t_HASHTAG   = r'\#'
+t_DOT       = r'\.'
+t_QUOTES    = r'\"'
+t_APOSTROPHE = r'\''
+
+def t_ARROW_FUNCTION(t):
+    r'=>'
+    return t
+
+def t_INCREMENT(t):
+    r'\+='
+    return t
+
+def t_LESSEQUAL(t):
+    r'<='
+    return t
+
+def t_GREATEREQUAL(t):
+    r'>='
+    return t
+
+def t_DEQUAL(t):
+    r'!='
+    return t
+
+def t_ISEQUAL(t):
+    r'=='
+    return t
+
+def t_ISEQUALTYPE(t):
+    r'==='
+    return t
+
+def t_MINUSMINUS(t):
+    r'--'
+    return t
+
+def t_PLUSPLUS(t):
+    r'\+\+'
+    return t
+
+def t_TIMESTIMES(t):
+    r'\*\*'
+    return t
+
+def t_DOT_DOT(t):
+    r'::'
+    return t
+
+
+# RE OTHERS
+
+
+def t_COMMENTS(t):
+    r'\/\*([^*]|\*[^\/])*(\*)+\/'
+    t.lexer.lineno += t.value.count('\n')
+
+def t_COMMENTS_C99(t):
+    r'(\/\/|\#)(.)*?\n'
+    t.lexer.lineno += 1
+
+def t_NUM(t):
+    r'\d+(\.\d+)?'
+    t.value = float(t.value)
+    return t
+
+def t_ID(t):
+    r'\w+(\w\d)*'
+    return t
+
+def t_STRING(t):
+    r'(("[^"]*")|(\'[^\']*\')|(\`[^\`]*\`))'
+    return t
+
+
+lexer = lex.lex()
+
+if __name__ == '__main__':
+    if (len(sys.argv) > 1):
+        script = sys.argv[1]
+
+        scriptfile = open(script, 'r')
+        scriptdata = scriptfile.read()
+        #print (scriptdata)
+        lexer.input(scriptdata)
+
+        print (chr(27)+"[0;36m"+"INICIA ANALISIS LEXICO"+chr(27)+"[0m")
+        i = 1
+        while True:
+            tok = lexer.token()
+            if not tok:
+                break
+            print ("\t"+str(i)+" - "+"Line: "+str(tok.lineno)+"\t"+str(tok.type)+"\t-->  "+str(tok.value))
+            i += 1
+
+        print (chr(27)+"[0;36m"+"TERMINA ANALISIS LEXICO"+chr(27)+"[0m")
+
+    else:
+        print (chr(27)+"[0;31m"+"Pase el archivo de script JAVASCRIPT como parametro:")
+        print (chr(27)+"[0;36m"+"\t$ python js_lexer.py"+chr(27)+"[1;31m"+" <filename>.js"+chr(27)+"[0m")
 
